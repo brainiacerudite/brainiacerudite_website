7035845f68a0cd2c56d9b85691718247829a2719
Brainiac <brainiacerudite@gmail.com>
2024-09-10 16:13:03 +0100

    set tailwind config colors

--- src/App.vue
+++ src/App.vue
@@ -8,35 +8,8 @@
   </header>
 
   <main>
-    <div class="text-blue-500">Main</div>
+    <div class="bg-dark-input-background-color">Main</div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      "input-placeholder-color": "#9E9E9E",
+      dark: {
+        "heading-color": "#F5F5F5",
+        "secondary-color": "#2A2A48",
+        "text-color": "#9E9E9E",
+        "primary-color": "#317DC6",
+        "background-color": "#030411",
+        "input-background-color": "#03041150",
+      },
+      light: {
+        "heading-color": "#1F1F1F",
+        "secondary-color": "#FFFFFF",
+        "text-color": "#5E5E5E",
+        "primary-color": "#317DC6",
+        "background-color": "#F5F5F5",
+        "input-background-color": "#F5F5F5",
+      },
+    },
     extend: {},
   },
   plugins: [],
