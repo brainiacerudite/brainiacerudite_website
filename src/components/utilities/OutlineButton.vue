155a0fc6c281116cc9fe526d4c7a538bd61b09a8
Brainiac <brainiacerudite@gmail.com>
2024-09-18 17:51:26 +0100

    Update api url

--- src/components/utilities/OutlineButton.vue
+++ src/components/utilities/OutlineButton.vue
@@ -7,11 +7,11 @@ const props = defineProps({
 
 <template>
     <a v-if="link" :href="href"
-        class="mb-2 w-full px-7 py-3 uppercase border border-light-heading-color border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-light-heading-color hover:text-light-background-color hover:bg-light-heading-color dark:border-dark-heading-color dark:text-dark-heading-color dark:hover:text-dark-background-color dark:hover:bg-dark-heading-color lg:mb-0 lg:max-w-max">
+        class="mb-2 w-full text-center block px-7 py-3 uppercase border border-light-heading-color border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-light-heading-color hover:text-light-background-color hover:bg-light-heading-color dark:border-dark-heading-color dark:text-dark-heading-color dark:hover:text-dark-background-color dark:hover:bg-dark-heading-color lg:mb-0 lg:max-w-max">
         <slot />
     </a>
     <button v-else
-        class="mb-2 w-full px-7 py-3 uppercase border border-light-heading-color border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-light-heading-color hover:text-light-background-color hover:bg-light-heading-color dark:border-dark-heading-color dark:text-dark-heading-color dark:hover:text-dark-background-color dark:hover:bg-dark-heading-color lg:mb-0 lg:max-w-max">
+        class="mb-2 w-full text-center block px-7 py-3 uppercase border border-light-heading-color border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-light-heading-color hover:text-light-background-color hover:bg-light-heading-color dark:border-dark-heading-color dark:text-dark-heading-color dark:hover:text-dark-background-color dark:hover:bg-dark-heading-color lg:mb-0 lg:max-w-max">
         <slot />
     </button>
 </template>
--- src/components/utilities/PrimaryButton.vue
+++ src/components/utilities/PrimaryButton.vue
@@ -7,11 +7,11 @@ const props = defineProps({
 
 <template>
     <a v-if="link" :href="href"
-        class="mb-2 w-full px-7 py-3 uppercase border border-transparent border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-dark-heading-color bg-light-primary-color hover:opacity-95 dark:text-dark-heading-color dark:bg-dark-primary-color lg:mb-0 lg:max-w-max">
+        class="mb-2 w-full text-center block px-7 py-3 uppercase border border-transparent border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-dark-heading-color bg-light-primary-color hover:opacity-95 dark:text-dark-heading-color dark:bg-dark-primary-color lg:mb-0 lg:max-w-max">
         <slot />
     </a>
     <button v-else
-        class="mb-2 w-full px-7 py-3 uppercase border border-transparent border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-dark-heading-color bg-light-primary-color hover:opacity-95 dark:text-dark-heading-color dark:bg-dark-primary-color lg:mb-0 lg:max-w-max">
+        class="mb-2 w-full text-center block px-7 py-3 uppercase border border-transparent border-solid transition-all duration-200 ease-in-out text-base font-semibold rounded text-dark-heading-color bg-light-primary-color hover:opacity-95 dark:text-dark-heading-color dark:bg-dark-primary-color lg:mb-0 lg:max-w-max">
         <slot />
     </button>
 </template>
